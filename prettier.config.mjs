3a935cca3dfbcf51745d2fcfc350904bb0cc25f8
omiso <omiso3tty@gmail.com>
2025-03-09 11:44:48 +0900

    feat: prettier導入

---
+++ prettier.config.mjs
@@ -0,0 +1,15 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  objectWrap: 'collapse',
+  endOfLine: 'lf',
+}
+
+export default config
