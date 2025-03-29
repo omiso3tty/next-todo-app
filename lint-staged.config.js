6a193cea8013c4280c02fd09252ff26bb5b47195
omiso <omiso3tty@gmail.com>
2025-03-29 12:25:33 +0900

    feat: markuplint導入

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   '**/*.{ts,tsx,js,jsx,cjs,mjs}': ['eslint --fix'],
+  '**/*.{tsx,jsx}': ['markuplint'],
   '**/*.{ts,tsx,js,jsx,cjs,mjs,json,yml,yaml}': ['prettier --write'],
 }
